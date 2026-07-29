9a8d86936b99a597a2a57020acb5aac20e31d020
Usman84kgg <usmanbinancep2p@gmail.com>
2026-07-29 22:52:33 +0300

    Update supabase.ts

--- src/lib/db/supabase.ts
+++ src/lib/db/supabase.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-// Эти ключи открытые, их можно хранить в коде
 const supabaseUrl = "https://lewytkfykerbxgtnyljv.supabase.co";
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxld3l0a2Z5a2VyYnhndG55bGp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzMzE3NTMsImV4cCI6MjEwMDkwNzc1M30.CEarN_1JkaKiE4TSfE7va9rzw6FTNNZVt9FRfzORqU8";
 
-export const supabase = createClient(supabaseUrl, supabaseKey || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxld3l0a2Z5a2VyYnhncm55bGp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEzMjYwMDAsImV4cCI6MjA2NjkwMjAwMH0.placeholder");
+export const supabase = createClient(supabaseUrl, supabaseKey);
